0e1f274707fa762a3af6c38cea970292b49f7213
srupat <srujanpatwardhan@gmail.com>
2023-06-06 08:14:30 +0530

    removed unnecessary comments

--- project.c
+++ project.c
@@ -1,34 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-// search specific patient
-// cancel appointment
-// queue size display
+
 struct Node * front = NULL;
 struct Node * rear = NULL;
-// struct patient
-// {
-//     char name[50];
-//     int urgency;
-//     char description[100];
-// };
+
 struct Node 
 {
-    //struct patient * data;
+    
     int age;
     int sev;
     struct Node * next;
 };
 
-// void newPatient()
-// {
-//     struct Node * p = (struct Node *)malloc(sizeof(struct Node));
-//     printf("Enter the name of the patient\n");
-//     fgets(p->data->name,100,stdin);
-//     printf("Enter the age of the patient \n");
-//     scanf("%d",p->data->age);
-//     printf("Enter the problem\n");
-//     fgets(p->data->description,100,stdin);
-// }
+
 void queueTraversal(struct Node * ptr)
 {
     printf("\nPrinting the patients who have taken appointment according to priority\n");
@@ -159,7 +143,6 @@ void cancelAppointment(int age, int severity)
                 ptr = ptr->next;
                 free(ptr);
             }  
-
         }
         }
     }
